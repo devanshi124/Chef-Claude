b0b11c6b5d66d494c2af87228e7bb3aa1fd72e4f
Devanshi <devanshit584@gmail.com>
2025-05-27 18:04:34 +0530

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,11 @@
+
+import chefClaudeLogo from './images/chef.png'
+export default function Header(){
+
+    return (
+        <header>
+            <img src={chefClaudeLogo}/>
+            <h2>Chef Dev</h2>
+        </header>
+    )
+}
